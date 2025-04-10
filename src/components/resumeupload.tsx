326e12483bc41a130aaad50b19a02ab111184442
vepaan <141347566+vepaan@users.noreply.github.com>
2025-04-10 17:30:02 -0500

    raw parser built

--- src/components/resumeupload.tsx
+++ src/components/resumeupload.tsx
@@ -1,17 +1,35 @@
 'use client'
 
+import { parse } from 'path';
 import React, {useState} from 'react';
 
 function ResumeUpload() {
     const [file, setFile] = useState<File | null>(null);
+    const [fileContent, setFileContent] = useState<string | null>(null);
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const selectedFile = e.target.files?.[0];
+
         if (selectedFile) {
             setFile(selectedFile);
+            parseFile(selectedFile);
+
         } else {
             console.log("smth wrong with file, check function handleFileChange")
         }
+    }
+
+    const parseFile = (selectedFile: File) => {
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setFileContent(reader.result as string);
+        }
+        reader.onerror = () => {
+            console.log('Error reading file, look up parseFile');
+        }
+
+        reader.readAsText(selectedFile);
     }
 
     return (
@@ -28,11 +46,20 @@ function ResumeUpload() {
                 onChange={handleFileChange}
                 className="hidden"
             />
+
             {file && (
                 <div className="mt-4">
                 <p className="text-gray-700">File selected: {file.name}</p>
                 </div>
             )}
+
+            {fileContent && (
+                <div className="mt-4 w-full max-w-4xl overflow-y-auto" style={{ maxHeight: '60vh' }}>
+                    <h3 className="text-xl">File Content:</h3>
+                    <pre className="whitespace-pre-wrap break-words">{fileContent}</pre>
+                </div>
+            )}
+
             </div>
     )
 };
