56bc65c1bbfe02689fce73d9d0fd627f9412aecb
arjavlamsal <lamsalarjav@gmail.com>
2025-04-13 13:56:52 -0500

    setuo shadcn, root, hero

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ResumeUpload from "./components/resumeupload";
-
-export default function Home() {
-  return (
-    <div className="w-full h-screen bg-white">
-      <ResumeUpload/>
-    </div>
-  );
-}
