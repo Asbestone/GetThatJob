f3abc76960ea83c51126db4594f881beeaf4ac17
vepaan <141347566+vepaan@users.noreply.github.com>
2025-04-14 11:15:09 -0500

    adding ai boilerplate initialized

--- src/app/api/cleanparsed/route.ts
+++ src/app/api/cleanparsed/route.ts
@@ -1,13 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({apiKey: "sdfsdf"});
-
 export async function POST(req: NextRequest) {
     console.log("Cleaning parsed data and returning as json");
+    const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API});
+
     const cleanedText = await req.text();
 
+    const aiResponse = await ai.models.generateContent({
+        model: "gemini-2.0-flash-lite", // this model is ideal for our usecase with higher rpm and lower performance
+        contents: "explain how ai works in 10 words",
+    })
 
+    console.log(aiResponse.text);
+    console.log(cleanedText);
 
     return NextResponse.json({ cleanedText });
 }
